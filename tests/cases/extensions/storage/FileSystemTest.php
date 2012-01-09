5e817152300335885f2ed1a394a480929576ce02
Marius Kubilius <marius.kubilius@gmail.com>
2012-01-09 04:35:21 +0200

    Moving the FileSystemTest to correct folder

---
+++ tests/cases/extensions/storage/FileSystemTest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Lithium Filesystem: managing file uploads the easy way
+ *
+ * @copyright     Copyright 2012, Little Boy Genius (http://www.littleboygenius.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace li3_filesystem\tests\cases\extensions\storage;
+
+//use SplFileInfo;
+//use lithium\core\Libraries;
+use li3_filesystem\extensions\storage\FileSystem;
+
+class FileSystemTest extends \lithium\test\Unit {
+
+	public function setUp() {
+		FileSystem::reset();
+	}
+
+	public function tearDown() {
+		FileSystem::reset();
+	}
+	
+	public function testBasicFileSystemConfig() {
+		$result = FileSystem::config();
+		$this->assertFalse($result);
+
+		$config = array('default' => array('adapter' => '\some\adapter', 'filters' => array()));
+		$result = FileSystem::config($config);
+		$this->assertNull($result);
+
+		$expected = $config;
+		$result = FileSystem::config();
+		$this->assertEqual($expected, $result);
+
+		$result = FileSystem::reset();
+		$this->assertNull($result);
+
+		$config = array('default' => array('adapter' => '\some\adapter', 'filters' => array()));
+		FileSystem::config($config);
+
+		$result = FileSystem::config();
+		$expected = $config;
+		$this->assertEqual($expected, $result);
+
+		$result = FileSystem::reset();
+		$this->assertNull($result);
+
+		$config = array('default' => array(
+			'adapter' => '\some\adapter',
+			'filters' => array('Filter1', 'Filter2')
+		));
+		FileSystem::config($config);
+		$result = FileSystem::config();
+		$expected = $config;
+		$this->assertEqual($expected, $result);
+	}
+	
+}
+?>
