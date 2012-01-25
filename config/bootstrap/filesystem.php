24b47dfca4a92feb7e2262b67bb8f96e27585093
Marius Kubilius <marius.kubilius@gmail.com>
2012-01-25 21:29:37 +0200

    Sample config with strategies

--- config/bootstrap/filesystem.php
+++ config/bootstrap/filesystem.php
@@ -6,5 +6,18 @@
  * @license       http://opensource.org/licenses/bsd-license.php The BSD License
  */
 
+use li3_filesystem\storage\FileSystem;
+use lithium\core\Libraries;
 
+FileSystem::config(array(
+    'default' => array(
+        'adapter' => 'File',
+        'strategies' => array(
+        	'FileUpload' => array(
+				'allowed' => array('png', 'jpg'),	
+			),
+		),
+        'path' => Libraries::get(true, 'path') . '/webroot/img'
+    ),
+));
 ?>
