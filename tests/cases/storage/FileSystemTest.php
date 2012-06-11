8017b47a31fb2ad75eff167f4f5f05155f674478
Hans Donner <hans.donner@pobox.com>
2012-06-11 18:19:46 +0200

    Add mock adapter to unit test FileSystem

---
+++ tests/cases/storage/FileSystemTest.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace li3_filesystem\tests\cases\storage;
+
+/**
+ * Lithium Filesystem: managing file uploads the easy way
+ *
+ * @copyright     Copyright 2012, Little Boy Genius (http://www.littleboygenius.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+use li3_filesystem\extensions\storage\FileSystem;
+
+class FileSystemTest extends \lithium\test\Integration {
+
+	public function setUp() {
+		FileSystem::reset();
+	}
+
+	public function tearDown() {
+		FileSystem::reset();
+	}
+
+	public function testBasicFileSystemConfig() {
+		$result = FileSystem::config();
+		$this->assertFalse($result);
+
+		$config = array('default' => array('adapter' => '\some\adapter', 'filters' => array()));
+		$result = FileSystem::config($config);
+		$this->assertNull($result);
+
+		$expected = $config;
+		$result   = FileSystem::config();
+		$this->assertEqual($expected, $result);
+
+		$result = FileSystem::reset();
+		$this->assertNull($result);
+
+		$config = array('default' => array('adapter' => '\some\adapter', 'filters' => array()));
+		FileSystem::config($config);
+
+		$result   = FileSystem::config();
+		$expected = $config;
+		$this->assertEqual($expected, $result);
+
+		$result = FileSystem::reset();
+		$this->assertNull($result);
+
+		$config = array('default' => array(
+				'adapter' => '\some\adapter',
+				'filters' => array('Filter1', 'Filter2')
+		));
+		FileSystem::config($config);
+
+		$result   = FileSystem::config();
+		$expected = $config;
+		$this->assertEqual($expected, $result);
+	}
+
+	public function testFileSystemWrite() {
+		$config = array('default' => array(
+				'adapter' => '\li3_filesystem\tests\mocks\adapter\storage\filesystem\Mock',
+				'filters' => array(),
+				'path'    => '/whatever/we/need'
+		));
+		FileSystem::config($config);
+
+		$result = FileSystem::config();
+		$this->assertEqual($config, $result);
+
+		$filename = 'test_file';
+		$data     = 'Some test content';
+
+		$this->assertTrue(FileSystem::write('default', $filename, $data));
+		$this->assertFalse(FileSystem::write('non_existing', $filename, $data));
+	}
+
+	public function testFileSystemRead() {
+		$config = array('default' => array(
+				'adapter' => '\li3_filesystem\tests\mocks\adapter\storage\filesystem\Mock',
+				'filters' => array(),
+				'path'    => '/whatever/we/need'
+		));
+
+		FileSystem::config($config);
+		$result = FileSystem::config();
+		$this->assertEqual($config, $result);
+
+		$filename = 'test_file';
+		$data     = 'Some Test content';
+
+		$result = FileSystem::write('default', $filename, $data);
+		$this->assertTrue($result);
+
+		$result = FileSystem::read('default', $filename);
+		$this->assertEqual($data, $result);
+	}
+}
+
+?>
